2587d7bec2391384897812c425b3da8f2435e04c
Simulation42 <c.alex0706@gmail.com>
2021-05-25 19:34:22 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  
+
+  let content = (
+    <div className="app">
+      <div className="titleCon">
+        <span className="title">ERIC ANDERSON</span>    
+      </div>
+    </div>
+  )
+
+  return content;
+
+}
+
+export default App;
